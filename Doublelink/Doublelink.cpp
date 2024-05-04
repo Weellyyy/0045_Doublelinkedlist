d9324b0ca6398f9b2b831d89f1df5819be8ea981
Weellyyy <wildanprtm2304@gmail.com>
2024-05-04 10:27:02 +0700

    menambahkan step 2

--- Doublelink/Doublelink.cpp
+++ Doublelink/Doublelink.cpp
@@ -13,4 +13,12 @@ Node* START = NULL;
 void addNode()
 {
 	Node* newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; // assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // assign value to the data field of the new node
+
+	// insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{// step 2: insert the new node at the beginning
 }
