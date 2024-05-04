19af9c8d29b413c1b4ce8dff7e0d13173fe40a74
Weellyyy <wildanprtm2304@gmail.com>
2024-05-04 10:43:50 +0700

    menambahkan bool listemty dan void traverse

--- Doublelink/Doublelink.cpp
+++ Doublelink/Doublelink.cpp
@@ -128,3 +128,25 @@ void deleteNode()
 	cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\1b[0m" << endl;
 
 }
+
+bool listEmty()
+    {
+        return (START == NULL);
+    }
+
+    void traverse()
+    {
+        if (listEmty())
+            cout << "\nList is emty" << endl;
+        else
+        {
+            cout << "\nRecord in accending order of roll number are:" << endl;
+            Node* currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << " " << currentNode->name << endl;
+                currentNode = currentNode->next;
+            }
+        }
+    }
+
